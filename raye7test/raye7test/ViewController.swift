134fe9eb38200f4354d9a93210f32525faddf584
Saeed <saeedhshm@gmail.com>
2019-11-13 11:14:44 +0200

    init the project

---
+++ raye7test/raye7test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  raye7test
+//
+//  Created by Mac on 11/13/19.
+//  Copyright © 2019 com.interteleco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
